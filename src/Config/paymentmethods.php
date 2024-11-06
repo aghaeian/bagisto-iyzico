01184a8a45d0daddc2f720abd124bfee12f4afb6
Hamed <115875025+aghaeian@users.noreply.github.com>
2024-11-06 19:00:22 +0300

    Create paymentmethods.php

---
+++ src/Config/paymentmethods.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'iyzico' => [
+        'code' => 'iyzico',
+        'title' => 'Iyzico',
+        'description' => 'Iyzico Payment Gateway',
+        'image' => asset('/vendor/aghaeian/iyzico/assets/images/iyzico.png'),
+        'class' => 'Aghaeian\Iyzico\Payment\Iyzico',
+        'active' => true,
+        'sort'=> 5,
+    ]
+];
