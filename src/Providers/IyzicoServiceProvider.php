bb2c083064b813ac3d337ae2535af0269ab47329
Hamed <115875025+aghaeian@users.noreply.github.com>
2024-11-06 19:03:59 +0300

    Create IyzicoServiceProvider.php

---
+++ src/Providers/IyzicoServiceProvider.php
@@ -0,0 +1,64 @@
+<?php
+ 
+namespace Aghaeian\Iyzico\Providers;
+
+use Illuminate\Support\ServiceProvider;
+ 
+/**
+ *  Iyzico service provider
+ *
+ * @author  aghaeian
+ */
+class IyzicoServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    
+   
+    public function boot()
+    {
+		
+        $this->loadRoutesFrom(__DIR__ . '/../Routes/web.php');
+
+        $this->loadTranslationsFrom(__DIR__ . '/../Resources/lang', 'iyzico');
+
+        $this->loadViewsFrom(__DIR__ . '/../Resources/views', 'iyzico');
+		
+        $this->publishes([
+            __DIR__ . '/../Resources/assets' => public_path('/vendor/aghaeian/iyzico/assets'),
+        ], 'iyzico');
+
+    }
+ 
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {   
+        $this->registerConfig();
+    }
+
+    /**
+     * Register package config.
+     *
+     * @return void
+     */
+    protected function registerConfig()
+    {   
+        //this will merge payment method
+        $this->mergeConfigFrom(
+            dirname(__DIR__) . '/Config/paymentmethods.php', 'payment_methods'
+        );
+
+        // add menu inside configuration  
+        $this->mergeConfigFrom(
+            dirname(__DIR__) . '/Config/system.php', 'core'   
+        );
+
+    }
+}
