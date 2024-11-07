f2ceb6a5ccc7f8187e0946e57e79404ffff94693
Hamed <115875025+aghaeian@users.noreply.github.com>
2024-11-07 18:26:03 +0300

    Update IyzicoConfig.php

--- src/Http/Controllers/IyzicoConfig.php
+++ src/Http/Controllers/IyzicoConfig.php
@@ -1,18 +1,17 @@
 <?php
 
-namespace Aghaeian\Iyzico\Http\Controllers;
+namespace Aghaeian\ZiraatBankasi\Http\Controllers;
 
 use ZiraatBankasiSDK\Options;
 
-class IyzicoConfig
+class ZiraatBankasiConfig
 {
-
     public function options()
     {
         $options = new Options();
-        $merchantId = core()->getConfigData('sales.payment_methods.iyzico.merchant_id');
-        $terminalId = core()->getConfigData('sales.payment_methods.iyzico.terminal_id');
-        $storeKey   = core()->getConfigData('sales.payment_methods.iyzico.store_key');
+        $merchantId = core()->getConfigData('sales.payment_methods.ziraatbankasi.merchant_id');
+        $terminalId = core()->getConfigData('sales.payment_methods.ziraatbankasi.terminal_id');
+        $storeKey   = core()->getConfigData('sales.payment_methods.ziraatbankasi.store_key');
 
         $options->setMerchantId($merchantId);
         $options->setTerminalId($terminalId);
@@ -29,7 +28,7 @@ class IyzicoConfig
      */
     public function environment()
     { 
-        $sandboxMode = core()->getConfigData('sales.payment_methods.iyzico.sandbox');
+        $sandboxMode = core()->getConfigData('sales.payment_methods.ziraatbankasi.sandbox');
 
         if ($sandboxMode) {			
             return "https://test.ziraatbank.com.tr/api";
