bfd282b49e074d8622aa6234058daaabac75f184
Hamed <115875025+aghaeian@users.noreply.github.com>
2024-11-07 17:50:44 +0300

    Update web.php

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Middleware\VerifyCsrfToken;
 use Aghaeian\Iyzico\Http\Controllers\IyzicoController;
 
 Route::group(['middleware' => ['web', 'theme', 'locale', 'currency']], function () {
-    // Ödeme formunu gösteren rota
+    // Ödeme formunu gösteren rota (yeni rota ismiyle)
     Route::get('iyzico-payment', [
         IyzicoController::class, 'showPaymentForm'])->name('iyzico.payment.form');
 
@@ -13,4 +13,8 @@ Route::group(['middleware' => ['web', 'theme', 'locale', 'currency']], function 
     Route::post('iyzico-payment', [
         IyzicoController::class, 'processPayment'
     ])->withoutMiddleware(VerifyCsrfToken::class)->name('iyzico.payment.process');
+
+    // Eski rota ismiyle ödeme formunu gösteren rota (hata alan kodlar için)
+    Route::get('iyzico-payment-checkout', [
+        IyzicoController::class, 'showPaymentForm'])->name('iyzico.payment.checkout');
 });
