ca0f1e5e75682fed234c88208c83c501322375d6
Hamed <115875025+aghaeian@users.noreply.github.com>
2024-11-07 18:20:39 +0300

    Update payment-form.blade.php

--- src/Resources/views/iyzico/payment-form.blade.php
+++ src/Resources/views/iyzico/payment-form.blade.php
@@ -7,20 +7,21 @@
 <body>
     <form method="POST" action="{{ route('iyzico.payment.process') }}">
         @csrf
+
         <label for="card_holder_name">Kart Sahibi Adı:</label>
-        <input type="text" name="card_holder_name" required>
+        <input type="text" id="card_holder_name" name="card_holder_name" required>
 
         <label for="card_number">Kart Numarası:</label>
-        <input type="text" name="card_number" required>
+        <input type="text" id="card_number" name="card_number" required>
 
         <label for="expire_month">Son Kullanma Ayı (MM):</label>
-        <input type="text" name="expire_month" required>
+        <input type="text" id="expire_month" name="expire_month" required>
 
         <label for="expire_year">Son Kullanma Yılı (YY):</label>
-        <input type="text" name="expire_year" required>
+        <input type="text" id="expire_year" name="expire_year" required>
 
         <label for="cvv">CVV:</label>
-        <input type="text" name="cvv" required>
+        <input type="text" id="cvv" name="cvv" required>
 
         <button type="submit">Ödemeyi Tamamla</button>
     </form>
