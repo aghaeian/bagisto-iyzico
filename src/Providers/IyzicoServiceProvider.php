4a34f4842f01a3ea4c9afb6e61b7052933269551
Hamed <115875025+aghaeian@users.noreply.github.com>
2024-11-07 18:01:13 +0300

    Update IyzicoServiceProvider.php

--- src/Providers/IyzicoServiceProvider.php
+++ src/Providers/IyzicoServiceProvider.php
@@ -1,13 +1,13 @@
 <?php
- 
+
 namespace Aghaeian\Iyzico\Providers;
 
 use Illuminate\Support\ServiceProvider;
- 
+
 /**
- *  Iyzico service provider
+ * Iyzico service provider
  *
- * @author  aghaeian
+ * @author  aghae
  */
 class IyzicoServiceProvider extends ServiceProvider
 {
@@ -16,30 +16,26 @@ class IyzicoServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    
-   
     public function boot()
     {
-		
         $this->loadRoutesFrom(__DIR__ . '/../Routes/web.php');
 
         $this->loadTranslationsFrom(__DIR__ . '/../Resources/lang', 'iyzico');
 
         $this->loadViewsFrom(__DIR__ . '/../Resources/views', 'iyzico');
-		
-        $this->publishes([
-            __DIR__ . '/../Resources/assets' => public_path('/vendor/aghaeian/iyzico/assets'),
-        ], 'iyzico');
 
+        $this->publishes([
+            __DIR__ . '/../Resources/assets' => public_path('vendor/aghaeian/iyzico/assets'),
+        ], 'iyzico');
     }
- 
+
     /**
      * Register services.
      *
      * @return void
      */
     public function register()
-    {   
+    {
         $this->registerConfig();
     }
 
@@ -49,16 +45,17 @@ class IyzicoServiceProvider extends ServiceProvider
      * @return void
      */
     protected function registerConfig()
-    {   
-        //this will merge payment method
+    {
+        // Ödeme yöntemi yapılandırmasını birleştirir
         $this->mergeConfigFrom(
-            dirname(__DIR__) . '/Config/paymentmethods.php', 'payment_methods'
+            dirname(__DIR__) . '/Config/paymentmethods.php',
+            'payment_methods'
         );
 
-        // add menu inside configuration  
+        // Sistem yapılandırmasını birleştirir
         $this->mergeConfigFrom(
-            dirname(__DIR__) . '/Config/system.php', 'core'   
+            dirname(__DIR__) . '/Config/system.php',
+            'core'
         );
-
     }
 }
